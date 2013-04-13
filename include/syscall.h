2f2b31929a9edd67b4ebbd0ce3e245932d7f9621
raf38 <a@b.c>
2013-04-13 11:44:34 -0400

    fixed syntax error

--- include/syscall.h
+++ include/syscall.h
@@ -5,7 +5,7 @@
 #include <linux/time.h>
 #include <linux/fcntl.h>
 
-#long asyscall6 (long a, long b, long c, long d, long e, long f);
+//long asyscall6 (long a, long b, long c, long d, long e, long f);
 void exit      (int status);
 long nanosleep (const struct timespec* req, struct timespec* rem);
 long write     (long fd, const void* buffer, long length);
